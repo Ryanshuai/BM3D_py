14bf9b6ed2a454989b667ae40e9286501453a525
Ryan <1018718155@qq.com>
2019-10-14 15:22:17 -0700

    BM_real_im_test finally passed!!! LOL

--- history_of_test/BM_real_im_test.py
+++ history_of_test/BM_real_im_test.py
@@ -32,12 +32,9 @@ def my_precompute_BM(img, kHW, NHW, nHW, tauMatch):
         for dj in range(-nHW, nHW + 1):
             ddk = (di + nHW) * Ns + dj + nHW
             t_img = translation_2d_mat(img, right=-dj, down=-di)
-            diff_table = img - t_img
             diff_table_2 = (img - t_img) * (img - t_img) * diff_margin
 
             sum_diff_2 = np.matmul(np.matmul(add_mat, diff_table_2), add_mat.T)
-            if di == -1 and dj == 1:
-                print(di, dj, sum_diff_2[103, 103])
             sum_table[ddk] = np.maximum(sum_diff_2, sum_margin)
 
     sum_table = sum_table.reshape((Ns * Ns, height * width))  # di_dj, ph_pw
@@ -57,8 +54,8 @@ def my_precompute_BM(img, kHW, NHW, nHW, tauMatch):
     sum_filter = np.where(sum_table_T < threshold, 1, 0)
     threshold_count = np.sum(sum_filter, axis=1)
 
-    # return Pr_N__Pnear, threshold_count
-    return Pr_N__Pnear, sum_table_T, argsort_di, argsort_dj, threshold_count
+    return Pr_N__Pnear, threshold_count
+    # return Pr_N__Pnear, sum_table_T, argsort_di, argsort_dj, threshold_count
 
 
 def translation_2d_mat(mat, right, down):
@@ -71,16 +68,11 @@ if __name__ == '__main__':
     im = cv2.imread('Cameraman256.png', cv2.IMREAD_GRAYSCALE)
     im_w = im.shape[1]
 
-    kHW, NHW, nHW, tauMatch = 2, 5, 4, 1000
-    Pr_N__Pnear, sum_table_T, argsort_di, argsort_dj, threshold_count = my_precompute_BM(im, kHW=kHW, NHW=NHW, nHW=nHW,
-                                                                                         tauMatch=tauMatch)
-    ref_y, ref_x = 103, 103
-    Pr = ref_y * im_w + ref_x
+    kHW, NHW, nHW, tauMatch = 8, 10, 16, 1000
+    Pr_N__Pnear, threshold_count = my_precompute_BM(im, kHW=kHW, NHW=NHW, nHW=nHW, tauMatch=tauMatch)
 
-    search_area_sum_table = sum_table_T[Pr].reshape(2 * nHW + 1, 2 * nHW + 1)
-    print(im[ref_y - nHW:ref_y + nHW + 1, ref_x - nHW:ref_x + nHW + 1])
-    print('-------------------------')
-    print(search_area_sum_table)
+    ref_y, ref_x = 100, 100
+    Pr = ref_y * im_w + ref_x
 
     im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
     cv2.rectangle(im, (ref_x, ref_y), (ref_x + kHW, ref_y + kHW), color=(255, 0, 0), thickness=1)
@@ -93,16 +85,12 @@ if __name__ == '__main__':
     for i, Pnear in enumerate(Pr_N__Pnear[Pr]):
         if i == 0:
             continue
-        di = argsort_di[Pr][i]
-        dj = argsort_dj[Pr][i]
-        print(di, dj)
         if i > count:
             break
         y = Pnear // im_w
         x = Pnear % im_w
-        # print(y, x)
-        # cv2.rectangle(im, (x, y), (x + kHW, y + kHW), color=(0, 255, 0), thickness=1)
+        cv2.rectangle(im, (x, y), (x + kHW, y + kHW), color=(0, 255, 0), thickness=1)
 
     # cv2.imshow('im', im)
     # cv2.waitKey()
-    cv2.imwrite('test_im.png', im)
+    cv2.imwrite('BM_real_im_test.png', im)
